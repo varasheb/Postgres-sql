781ccba5aac6d62d31b2ece8d547d94f2558702b
varasheb <varkanthi456@gmail.com>
2024-05-27 11:13:59 +0530

    [Varshab] Add: system and built-in functions

--- function.sql
+++ function.sql
@@ -42,3 +42,52 @@ SELECT CONCAT(student_name, ' - ', address) FROM student; -- Concatenates studen
 
 ** SUBSTRING(): Returns a substring from a string.
 SELECT SUBSTRING(student_name FROM 1 FOR 3) FROM student; -- Returns the first 3 characters of student names
+
+
+>>>>>>>>>>>>>>>>>System Functions:<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+System functions in PostgreSQL are functions provided by the database system to perform various system-related tasks. They are used to get information about the database, server, or perform operations related to system settings.
+
+** NOW(): Returns the current date and time.
+SELECT NOW();
+
+** CURRENT_USER: Returns the name of the current user.
+SELECT CURRENT_USER;
+
+** VERSION(): Returns the version of the PostgreSQL server.
+SELECT VERSION();
+
+>>>>>>>>>>>>>>>>>>>Built-in Functions:<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+Built-in functions in PostgreSQL are functions that are included with the database and provide various functionalities for data manipulation, calculations, and conversions.
+
+** LENGTH(): Returns the length of a string.
+SELECT LENGTH('hello');
+
+** ABS(): Returns the absolute value of a number.
+SELECT ABS(-10);
+
+** TO_DATE(): Converts a string to a date.
+SELECT TO_DATE('2024-05-30', 'YYYY-MM-DD');
+
+>>>>>>>>>>>>>>>>>>>Window Functions:<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+Window functions in PostgreSQL are used to perform calculations across a set of rows related to the current row. They are often used in analytical queries to calculate moving averages, rank results, and perform other operations that require looking at neighboring rows.
+
+** ROW_NUMBER(): Assigns a unique sequential integer to each row within a partition of a result set.
+SELECT first_name, ROW_NUMBER() OVER (ORDER BY marks DESC) AS rank
+FROM student
+INNER JOIN marks ON student.student_id = marks.student_id
+ORDER BY rank;
+
+
+** LAG(): Accesses data from a previous row in the same result set without the use of a self-join.
+SELECT first_name, marks, LAG(marks) OVER (ORDER BY marks) AS prev_score
+FROM student
+INNER JOIN marks ON student.student_id = marks.student_id
+ORDER BY marks;
+
+
+** SUM() OVER(): Calculates a running sum over a set of rows.
+ SELECT student.student_id, first_name, SUM(marks) OVER (PARTITION BY student.student_id) AS total_marks
+FROM student
+INNER JOIN marks ON student.student_id = marks.student_id
+ORDER BY student_id;
+
